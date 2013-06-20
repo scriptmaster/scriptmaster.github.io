c10fbbfc3dcbff8a0170c64e0f0a1695e0b23ac7
ScriptMaster <scriptmaster@gmail.com>
2013-06-20 21:57:07 +0530

    Create moonjicomments.js

---
+++ fb/moonjicomments.js
@@ -0,0 +1,19 @@
+
+window.fbAsyncInit = function() {
+  if(location.search) {
+    var a = location.search.match(/[\?&]appId=(\d+)/);
+    a && a[1] && window.appId = a[1];
+  }
+
+  FB.init({
+		appId      : window.appId || 211807025608686, // App ID
+		channelUrl : '//'+location.host+'/channel.html', // Channel File
+		status     : true, // check login status
+		cookie     : true, // enable cookies to allow the server to access the session
+		xfbml      : true  // parse XFBML
+	});
+
+	FB.getLoginStatus(function(){
+		// 
+	});
+}
