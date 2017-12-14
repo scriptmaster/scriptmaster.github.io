60c3458d0e3f5a627584a110483e727dd65d04ab
ScriptMaster <scriptmaster@gmail.com>
2017-12-14 10:37:05 +0530

    Update all.js

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,7 +1,7 @@
 /* Sheriff's 2KB JS framework */
 doc = document, d = { id: 'getElementById', q: 'querySelector', qa: 'querySelectorAll' };
 on = function (el, ev, ef, c) { if (!el) return; ev = 'on' + ev; if (!(el instanceof Array)) el = [el]; el.forEach(function (e) { if (e[ev]) c = e[ev]; else c = function(){ }; e[ev] = function(e) { c.call(this,e); ef.call(this,e); } }) } // no attachEvent addEventHandler
-ready = function (f) { if (readies) readies.push(f); else f() }; readies = []; document.onreadystatechange = function (ev) { document.onreadystatechange = null; body = document.body; readies.forEach(function (f) { f() }); readies = null };
+ready = function (f) { if (readies) readies.push(f); else f() }; readies = []; document.onreadystatechange = function (ev) { document.onreadystatechange = null; body = document.body; readies.forEach(f); readies = null };
 id = function (e) { return doc[d.id](e) };
 first = function (e) { return doc[d.q](e) };
 all = function (e) { return doc[d.qa](e) };
@@ -15,4 +15,8 @@ ready(function () {
             location.href = '/pro';
         }
     });
+    // A/B testing, ab -c 10 -n 100 test, crazy world. //
+    if(Math.random() < 0.5) {
+        document.body.classList.add('stripe')
+    }
 });
