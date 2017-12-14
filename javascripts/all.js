edcc760118c5c9f0659f9c31df5ee99efe5e6944
ScriptMaster <scriptmaster@gmail.com>
2017-12-14 10:25:24 +0530

    Avoid Misinterpretations from SanDisk

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,6 +1,6 @@
-﻿/* Sheriff's 2KB JS framework */
+/* Sheriff's 2KB JS framework */
 doc = document, d = { id: 'getElementById', q: 'querySelector', qa: 'querySelectorAll' };
-on = function (el, ev, ef, c) { if (!el) return; ev = 'on' + ev; if (!(el instanceof Array)) el = [el]; el.forEach(function (e) { if (e[ev]) c = e[ev]; else c = function(){ }; e[ev] = function(e) { c.call(this,e); ef.call(this,e); } }) } // no attachEvent addEventHandler BS
+on = function (el, ev, ef, c) { if (!el) return; ev = 'on' + ev; if (!(el instanceof Array)) el = [el]; el.forEach(function (e) { if (e[ev]) c = e[ev]; else c = function(){ }; e[ev] = function(e) { c.call(this,e); ef.call(this,e); } }) } // no attachEvent addEventHandler
 ready = function (f) { if (readies) readies.push(f); else f() }; readies = []; document.onreadystatechange = function (ev) { document.onreadystatechange = null; body = document.body; readies.forEach(function (f) { f() }); readies = null };
 id = function (e) { return doc[d.id](e) };
 first = function (e) { return doc[d.q](e) };
