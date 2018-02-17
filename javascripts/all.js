316a9e180c882bc9eb6e4048f5820091aa87ef12
ScriptMaster <scriptmaster@gmail.com>
2018-02-18 01:55:52 +0530

    Update all.js

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,13 +1,16 @@
-/* Sheriff's 2KB JS framework */
-doc = document, d = { id: 'getElementById', q: 'querySelector', qa: 'querySelectorAll' };
-on = function (el, ev, ef, c) { if (!el) return; ev = 'on' + ev; if (!(el instanceof Array)) el = [el]; el.forEach(function (e) { if (e[ev]) c = e[ev]; else c = function(){ }; e[ev] = function(e) { c.call(this,e); ef.call(this,e); } }) } // no attachEvent addEventHandler
-ready = function (f) { if (readies) readies.push(f); else f() }; readies = []; document.onreadystatechange = function (ev) { document.onreadystatechange = null; body = document.body; readies.forEach(function(f){f()}); readies = null };
-id = function (e) { return doc[d.id](e) };
-first = function (e) { return doc[d.q](e) };
-all = function (e) { return doc[d.qa](e) };
-style = function (el, s, v) { if (el) { if (!(el instanceof Array)) el = [el]; el.forEach(function (e) { e.style[s] = v; }) } }
-create = function(e){return document.createElement(e)}
-/* Sheriff's 2KB JS framework */
+/* Sheriff's 1KB JS framework */
+(function(){
+var doc = document, d = { id: 'getElementById', q: 'querySelector', qa: 'querySelectorAll' };
+var w = window;
+w.on = function (el, ev, ef, c) { if (!el) return; ev = 'on' + ev; if (!(el instanceof Array)) el = [el]; el.forEach(function (e) { if (e[ev]) c = e[ev]; else c = function(){ }; e[ev] = function(e) { c.call(this,e); ef.call(this,e); } }) } // no attachEvent addEventHandler
+w.ready = function (f) { if (readies) readies.push(f); else f() }; readies = []; document.onreadystatechange = function (ev) { document.onreadystatechange = null; body = document.body; readies.forEach(function(f){f()}); readies = null };
+w.id = function (e) { return doc[d.id](e) };
+w.first = function (e) { return doc[d.q](e) };
+w.all = function (e) { return doc[d.qa](e) };
+w.style = function (el, s, v) { if (el) { if (!(el instanceof Array)) el = [el]; el.forEach(function (e) { e.style[s] = v; }) } }
+w.create = function(e){return document.createElement(e)}
+})()
+//class{constructor(){}}
 
 ready(function () {
     on(first('header'), 'click', function (e) {
@@ -15,7 +18,7 @@ ready(function () {
             location.href = '/pro';
         }
     });
-    // A/B testing, ab -c 10 -n 100 test, crazy world. //
+
     if(Math.random() < 0.5) {
         document.body.classList.add('stripe')
     }
