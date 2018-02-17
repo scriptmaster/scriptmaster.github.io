b2dd39ffdebadce7cd8ecd06ae10d5682ca96856
ScriptMaster <scriptmaster@gmail.com>
2018-02-18 01:56:23 +0530

    Update all.js

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,7 +1,6 @@
 /* Sheriff's 1KB JS framework */
 (function(){
-var doc = document, d = { id: 'getElementById', q: 'querySelector', qa: 'querySelectorAll' };
-var w = window;
+var doc = document, d = { id: 'getElementById', q: 'querySelector', qa: 'querySelectorAll' }, w = window;
 w.on = function (el, ev, ef, c) { if (!el) return; ev = 'on' + ev; if (!(el instanceof Array)) el = [el]; el.forEach(function (e) { if (e[ev]) c = e[ev]; else c = function(){ }; e[ev] = function(e) { c.call(this,e); ef.call(this,e); } }) } // no attachEvent addEventHandler
 w.ready = function (f) { if (readies) readies.push(f); else f() }; readies = []; document.onreadystatechange = function (ev) { document.onreadystatechange = null; body = document.body; readies.forEach(function(f){f()}); readies = null };
 w.id = function (e) { return doc[d.id](e) };
