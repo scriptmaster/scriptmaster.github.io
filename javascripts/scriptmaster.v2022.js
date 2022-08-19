973b3026a1e1f324d358f8cf8b566b8b71ebe34b
ScriptMaster <scriptmaster@gmail.com>
2022-08-19 20:30:24 +0530

    Update scriptmaster.v2022.js

--- javascripts/scriptmaster.v2022.js
+++ javascripts/scriptmaster.v2022.js
@@ -3,9 +3,11 @@
 const d = { id: 'getElementById', q: 'querySelector',qa:'querySelectorAll', aEL:'addEventListener',rEL:'removeEventListener' };
 w.on = function (el, ev, ef) { if(!el) return; if(typeof el=='string')el=all(el); if(!el.forEach)el=[el]; el.forEach(function(e){ e[d.aEL](ev, ef) }) }
 w.once = function(el,ev,ef){var c=function(p){el[d.rEL](ev,c);ef(p)};on(el,ev,c)}
+
 var starters = [];
 w.ready = function (f) { if (starters) starters.push(f); else f() };
 once(doc, 'DOMContentLoaded', function(){starters.forEach(function(f){f()});starters=null;})
+
 w.id = function (e) { return doc[d.id](e) };
 w.first = function (e) { return doc[d.q](e) };
 w.all = function (e) { return doc[d.qa](e) };
@@ -14,20 +16,35 @@ w.create = function(e){return document.createElement(e)}
 
 Node.prototype.extends = function(Cls) {
   if(!Cls) throw new Error('Required Parameter "Class" missing.');
-  const extensions = { $$: (function(s){ return this.querySelectorAll(s) }).bind(this) };
+  const extensions = {
+    $$: this.querySelectorAll.bind(this),
+    $: this.querySelector.bind(this),
+    on: function(ev, ef){on(this,ev,ef.bind(this))}.bind(this),
+    request: request,
+    fetch: (u,o) => { return request(u, { headers: { 'X-Requested-With': 'fetch' } }) },
+    xhr:   (u,o) => { return request(u, { headers: { 'X-Requested-With': 'XMLHttpRequest' } }) },
+  };
   const NewCls = Object.assign(Cls)
   Object.assign(NewCls.prototype, extensions);
   const o = new NewCls(Array.prototype.slice.call(arguments,1));
-  const r = Object.assign(this, o, extensions);
-  Object.getOwnPropertyNames(Cls.prototype).forEach(function(k){
-    if(k.length > 2 && k.substring(0,2)=='on') on(r, k.substring(2), o[k].bind(r));
-  })
+  const m = Object.assign(this, o, extensions); // merged Node
+  const opn=Object.getOwnPropertyNames(Cls.prototype); opn.forEach(p=>binder(p, o, m));
+  function binder(k, o, m, fn) {
+    if(k.length > 2 && k.substring(0,2)=='on') { on(m, k.substring(2), (fn || o[k]).bind(m)); }
+  };
+  if(o.members instanceof Array) o.members?.forEach(function(slctr){
+    const elt = m.$$(slctr), _slctr=slctr.replace(/[\\.-\s]/g,'_'); elt.forEach(el => opn.forEach(k => {
+      if(k.substring(0, _slctr.length) != _slctr) return;
+      if(k == _slctr) return o[k].call(m, el);
+      if(k[_slctr.length] == '_') return binder(k.substring(_slctr.length+1), o, Object.assign(el, o, extensions), o[k]);
+    }));
+  });
+  function request(url, opts, fn) { return (fn||fetch)(url, { credentials: 'same-origin', headers: {'X-Requested-With': 'XMLHttpRequest'}, ...opts }); }
 }
 
 NodeList.prototype.extends = function(Cls) {this.forEach(function(n){n.extends(Cls);}) }
 
-ready(function(){setTimeout(extendReady,10)})
-
+ready(extendReady);
 
 function extendReady() {
   all('.extends').forEach(function(el) {
@@ -40,8 +57,7 @@ function extendReady() {
     if(dbv && typeof w[dbv] == 'function') el.extends(w[dbv]);
   });
 }
-
 })(document,this)
 
-//class MyComponent{constructor(){}}; window['MyComponent']=MyComponent; MyOtherComp = class { onclick() { /*-*/ } }
-//HTML: <ul class="extends classMyComponent">...</ul>
+//class MyComponent{constructor(){} members = ['.btn']; _btn_onclick(){ } }
+//HTML: <ul class="extends classMyComponent"><li data-bind="li"></li></ul>
